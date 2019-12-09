87d1d7f2ce8bbb8369e5435303ff86f5db0f6d6a
Vishal ArorA <vishal.op.arora@gmail.com>
2019-12-09 23:16:27 +0530

    change

--- scr/replace.py
+++ scr/replace.py
@@ -2,61 +2,61 @@ import os
 import sys
 
 
-def replaceContent(fileName, oldText, newText):
-	fin = open(fileName, "rt")
-	data = fin.read()
-	data = data.replace(oldText, newText)
-	fin.close()
+def replaceContent(file_name, old_text, new_text):
+    fin = open(file_name, "rt")
+    data = fin.read()
+    data = data.replace(old_text, new_text)
+    fin.close()
 
-	fin = open(fileName, "wt")
-	fin.write(data)
-	fin.close()
-
-
-def getvCloudFolder(envVar):
-	envVar = os.environ[envVar]
-	vColudFolder = os.path.join(envVar, "vCloud")
-	return vColudFolder
+    fin = open(file_name, "wt")
+    fin.write(data)
+    fin.close()
 
 
-def getIPAddress(vColudFolder):
-	ipFilePath = os.path.join(vColudFolder, "ipFile.txt")
-	getEqual = False
-	ipAddress = []
-
-	with open(ipFilePath, 'r') as content:
-		data = content.read()
-		for each in data:
-			if getEqual:
-				ipAddress.append(each)
-			if each == "=":
-				getEqual = True
-
-	getIPAddress = "".join(ipAddress)
-	return getIPAddress
+def getvCloudFolder(env_var):
+    env_var = os.environ[env_var]
+    v_cloud_folder = os.path.join(env_var, "vCloud")
+    return v_cloud_folder
 
 
-def replaceIPAddress(envVariable, newIPAddress):
-	vColudFolder = getvCloudFolder(envVariable)
-	oldIPAddress = getIPAddress(vColudFolder)
+def getIPAddress(v_cloud_folder):
+    ip_file_path = os.path.join(v_cloud_folder, "ipFile.txt")
+    get_equal = False
+    ip_address = []
 
-	for root, directories, files in os.walk(vColudFolder):
-		for file in files:
-			fileFullPath = os.path.join(vColudFolder, file)
-			replaceContent(fileFullPath, oldIPAddress, newIPAddress)
+    with open(ip_file_path, 'r') as content:
+        data = content.read()
+        for each in data:
+            if get_equal:
+                ip_address.append(each)
+            if each == "=":
+                get_equal = True
+
+    get_ip_address = "".join(ip_address)
+    return get_ip_address
+
+
+def replaceIPAddress(env_variable, new_ip_address):
+    v_cloud_folder = getvCloudFolder(env_variable)
+    old_ip_address = getIPAddress(v_cloud_folder)
+
+    for root, directories, files in os.walk(v_cloud_folder):
+        for file in files:
+            file_full_path = os.path.join(v_cloud_folder, file)
+            replaceContent(file_full_path, old_ip_address, new_ip_address)
 
 
 if __name__ == '__main__':
-	#if len(sys.argv) > 1:
-	#	print(sys.argv[1])
+    # if len(sys.argv) > 1:
+    #	print(sys.argv[1])
 
-    #var = str(sys.argv[1])
-	print ("Hello")
-	var = "No"
+    # var = str(sys.argv[1])
+    print("Hello")
+    var = "No"
 
-	if var == "IP":
-		pass
-        #envVariable = str(sys.argv[2]) # environment Variable
-        #newIPAddress = str(sys.argv[3]) # New IP address
-        #replaceIPAddress(envVariable, newIPAddress)
-	pass
+    if var == "IP":
+        pass
+    # envVariable = str(sys.argv[2]) # environment Variable
+    # newIPAddress = str(sys.argv[3]) # New IP address
+    # replaceIPAddress(envVariable, newIPAddress)
+    pass
